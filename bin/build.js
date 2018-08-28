662fad8d3260038adc426073916ce6ae0e5bf803
David Sevilla <dsevilla192@icloud.com>
2018-08-28 19:05:53 -0400

    Fix build script, add default emojis.json

--- bin/build.js
+++ bin/build.js
@@ -1,7 +1,7 @@
+const build = require('../build');
+
 process.stdout.write(
     '\u001b[96m\u001b[1mGenerating simple emoji map...\u001b[0m'
 );
 
-require('../build').then(() =>
-    console.log('\u001b[96m\u001b[1m 👍\u001b[0m\n')
-);
+build().then(() => console.log('\u001b[96m\u001b[1m 👍\u001b[0m\n'));
